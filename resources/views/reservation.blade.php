3eb632c9d9c11948c4b97a9858de8e0763f2c5b6
zaki1003 <mz.merabet@esi-sba.dz>
2023-05-21 21:29:43 +0100

    Adding some changes

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h4>Attendance</h4>
+                        <h4>Reservations</h4>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
